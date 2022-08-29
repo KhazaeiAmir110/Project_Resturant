07c7a88151a69df3df03484d86e06002feaad860
test_python <khazaei.amir110@gmail.com>
2022-08-29 12:16:04 +0430

    Creat Views and HTML

--- foods/views.py
+++ foods/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Food
 
-# Create your views here.
+
+def food_list(request):
+    foods_list = Food.objects.all()
+    context = {
+        "foods": foods_list
+    }
+    return render(request, "foods/list.html", context)
+
+
+def food_detail(request, pk):
+    food = Food.objects.get(pk=pk)
+    context = {
+        "food": food
+    }
+    return render(request, "foods/detail.html", context)
