1ff926a1703c70327f7a6d238ee5ea7b80874007
test_python <khazaei.amir110@gmail.com>
2022-08-31 11:21:52 +0430

    Change the database and update front

--- foods/models.py
+++ foods/models.py
@@ -3,6 +3,11 @@ from django.utils.translation import gettext as _
 
 
 class Food(models.Model):
+    FOOD_TYPE = [
+        ("lunch", "تهار"),
+        ("dinner", "شام"),
+        ("drinks", "نوشیدنی")
+    ]
     name = models.CharField(max_length=50)
     description = models.CharField(_('توضیحات'), max_length=50)
     rate = models.IntegerField(_('امتیاز'), default=0)
@@ -10,6 +15,7 @@ class Food(models.Model):
     time = models.IntegerField(_('زمان'))
     pub_date = models.DateField(_('تاریخ انتشار'), auto_now=False, auto_now_add=True)
     photo = models.ImageField(upload_to='foods/')
+    food_type = models.CharField("نوع", max_length=10, choices=FOOD_TYPE, default="drinks")
 
     # نمایش غذا برای دیگران
     def __str__(self):
